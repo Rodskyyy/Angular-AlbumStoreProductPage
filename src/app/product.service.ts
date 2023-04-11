dcf4101b49dc6a47fee1b7b2ce10fcace44f15ef
Rodskyyy <rtabugon22@gmail.com>
2023-04-11 15:22:50 +0800

    Completed module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -9,7 +9,7 @@ export class ProductService {
 
   constructor(private _http: Http) { }
 
-  getAlbum (od: number)
+  getAlbum (id: number)
   {
     return this._http.get(this._albumUrl).map((response) => 
     response.json());
